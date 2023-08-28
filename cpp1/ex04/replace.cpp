42f3568f1d1fb0d9f1b6da7ab2e27a5a71dcb1f6
anvieira <anvieira@student.42porto.com>
2023-08-28 16:15:12 +0100

    definition fuctions

---
+++ cpp1/ex04/replace.cpp
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   replace.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anvieira <anvieira@student.42porto.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 14:37:45 by anvieira          #+#    #+#             */
+/*   Updated: 2023/08/28 16:12:07 by anvieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Replace.hpp"
+
+Replace::Replace( string filename, string s1, string s2 )
+{
+    this->filename = filename;
+    this->s1 = s1;
+    this->s2 = s2;
+    get_content();
+}
+
+Replace::~Replace()
+{
+}
+
+bool Replace::get_content( void )
+{
+    ifstream file;
+    string line;
+
+    file.open(this->filename.c_str());
+    if (file.is_open())
+    {
+        while (std::getline(file, line))
+        {
+            this->content += line;
+            this->content += '\n';
+        }
+        return true;
+    }
+    else
+    {
+        std::cout << "Error opening file" << std::endl;
+        return false;
+    }
+}
+
+void Replace::replace( void )
+{
+    size_t pos;
+
+    pos = this->content.find(this->s1);
+
+    if (pos != std::string::npos)
+    {
+        while (pos != std::string::npos)
+        {
+            this->content.erase(pos, this->s1.length());
+            this->content.insert(pos, this->s2);
+            pos = this->content.find(this->s1, pos + this->s2.length());
+        }
+    }
+    else
+        std::cout << "String: " << this->s1 << " not found" << std::endl;
+}
+
+void Replace::write_file ( void )
+{
+    ofstream file;
+    string filename_new = this->filename + ".replace";
+
+    file.open(filename_new.c_str());
+    if (file.is_open())
+    {
+        file << this->content;
+        file.close();
+    }
+    else
+        std::cout << "Error opening file" << std::endl;
+}

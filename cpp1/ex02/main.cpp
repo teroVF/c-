34e76440c8172d5e6916685c3d1961acc5ee68f4
anvieira <anvieira@student.42porto.com>
2023-08-28 03:15:42 +0100

    finish ex02

---
+++ cpp1/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anvieira <anvieira@student.42porto.com     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 04:39:54 by anvieira          #+#    #+#             */
+/*   Updated: 2023/08/27 18:03:29 by anvieira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "string"
+#include "iostream"
+
+int main(void)
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+    
+    std::cout << &str << std::endl;
+    std::cout << stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    std::cout << str << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
+    
+    return (0);
+}

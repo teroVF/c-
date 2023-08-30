6b210b5d970166454d491aad201b6d632e6f4c77
teroVF <anvieira@student.42porto.com>
2023-08-30 01:58:27 +0100

    put in cpp file the construtor and destructor

--- cpp1/ex03/HumanA.cpp
+++ cpp1/ex03/HumanA.cpp
@@ -3,14 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   HumanA.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anvieira <anvieira@student.42porto.com     +#+  +:+       +#+        */
+/*   By: anvieira <anvieira@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 00:43:10 by anvieira          #+#    #+#             */
-/*   Updated: 2023/08/28 01:51:36 by anvieira         ###   ########.fr       */
+/*   Updated: 2023/08/30 01:57:47 by anvieira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanA.hpp"
+
+HumanA::HumanA(std::string n, Weapon &w) : name(n), weapon(w) {/* void */}
+
+HumanA::~HumanA() {}
 
 void HumanA::attack( void )
 {
